673a8bc21b6065f79a94a3f31c1b3073f99c642d
esndmr <esndmr@gmail.com>
2021-02-11 19:29:03 +0300

    Add project files.

---
+++ İnterfaces/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace İnterfaces
+{
+    {
+        //İnterface newlenemez 
+        private static void Main(string[] args)
+    class Program
+        {
+            //IPersonManager customerManager = new CustumerManager();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new CustumerManager()); 
+
+        }
+    }
+
+    interface IPersonManager 
+    {
+        //unimplemented operation - tamamlanmamış operasyon
+        void Add();
+        void Update();
+
+
+    }
+    //inherits - class ......... İmplements - İnterface
+
+    class CustumerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //Müşteri ekleme kodları
+            Console.WriteLine("Müşteri eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi.");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        //Personel ekleme kodları
+        public void Add()
+        {
+            Console.WriteLine("Personel eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi.");
+        }
+    }
+    class ProjectManager
+    {
+        public void Add( IPersonManager personManager)
+        {
+            
+            personManager.Add();
+
+        }
+        
+    }
+}
